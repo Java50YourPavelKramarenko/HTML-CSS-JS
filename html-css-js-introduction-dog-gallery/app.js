780a42ec7fd3dccfcede59ce019a987d93464144
KramarenkoPavel <pavel.01kramarenko@gmail.com>
2022-12-23 08:27:00 +0200

    Edded HM#11

---
+++ html-css-js-introduction-dog-gallery/app.js
@@ -0,0 +1,11 @@
+console.log("Hello world!");
+let num = 25.325;
+num = Math.floor(num); //округление в сторону уменьшения
+num = Math.cell(num);//cell в сторону увеличения,round просто округление.
+num = Math.cell(num);//просто округление.
+num **=2; // num=num ** 2
+
+// strings
+let str = "a'bs'd";
+str = "abcd";
+let res = "123" + 56;
